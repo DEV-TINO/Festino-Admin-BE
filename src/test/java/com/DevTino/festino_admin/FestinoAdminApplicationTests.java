a40042897bebf6468aadffebf4737dc50cc93762
Lee <lsmin476@naver.com>
2024-06-25 01:58:55 +0900

    create: create Project

---
+++ src/test/java/com/DevTino/festino_admin/FestinoAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevTino.festino_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestinoAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
