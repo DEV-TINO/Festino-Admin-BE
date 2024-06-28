3d5a0e1c2fe9388e9f82d7641ba8667a563254da
YooSuHyeon <dbtngus20@naver.com>
2024-06-28 16:50:14 +0900

    fix / delete annotation

--- src/main/java/com/DevTino/festino_admin/booth/repository/FoodBoothRepository.java
+++ src/main/java/com/DevTino/festino_admin/booth/repository/FoodBoothRepository.java
@@ -2,10 +2,8 @@ package com.DevTino.festino_admin.booth.repository;
 
 import com.DevTino.festino_admin.booth.domain.FoodBoothDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@Repository
 public interface FoodBoothRepository extends JpaRepository<FoodBoothDAO, UUID> {
 }
