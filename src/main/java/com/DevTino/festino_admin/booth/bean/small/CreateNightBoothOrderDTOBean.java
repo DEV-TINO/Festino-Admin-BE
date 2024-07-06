dfda64630a224bc0090131f57a779f761fbde399
yoosuhyeon <dbtngus20@naver.com>
2024-07-06 23:14:05 +0900

    comment : reservation -> order

--- src/main/java/com/DevTino/festino_admin/booth/bean/small/CreateNightBoothOrderDTOBean.java
+++ src/main/java/com/DevTino/festino_admin/booth/bean/small/CreateNightBoothOrderDTOBean.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class CreateNightBoothOrderDTOBean {
 
-    // 야간부스 예약가능 여부 DTO 생성
+    // 야간부스 주문 가능 여부 DTO 생성
     public ResponseNightBoothOrderGetDTO exec(NightBoothDAO nightBoothDAO) {
         return ResponseNightBoothOrderGetDTO.builder()
                 .boothId(nightBoothDAO.getBoothId())
