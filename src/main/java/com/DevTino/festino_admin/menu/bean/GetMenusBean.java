827a594406df0347eb5132eebbc1e92b08414cdd
yoosuhyeon <dbtngus20@naver.com>
2024-07-06 23:04:50 +0900

    fix : GetMenuBean -> GetMenuDAOBean call

--- src/main/java/com/DevTino/festino_admin/menu/bean/GetMenusBean.java
+++ src/main/java/com/DevTino/festino_admin/menu/bean/GetMenusBean.java
@@ -1,6 +1,7 @@
 package com.DevTino.festino_admin.menu.bean;
 
 import com.DevTino.festino_admin.menu.bean.small.CreateMenusDTOBean;
+import com.DevTino.festino_admin.menu.bean.small.GetMenuDAOBean;
 import com.DevTino.festino_admin.menu.domain.DTO.ResponseMenusGetDTO;
 import com.DevTino.festino_admin.menu.domain.MenuDAO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,19 +12,19 @@ import java.util.UUID;
 
 @Component
 public class GetMenusBean {
-    GetMenuBean getMenuBean;
+    GetMenuDAOBean getMenuDAOBean;
     CreateMenusDTOBean createMenusDTOBean;
 
     @Autowired
-    public GetMenusBean(GetMenuBean getMenuBean, CreateMenusDTOBean createMenusDTOBean) {
-        this.getMenuBean = getMenuBean;
+    public GetMenusBean(GetMenuDAOBean getMenuDAOBean, CreateMenusDTOBean createMenusDTOBean) {
+        this.getMenuDAOBean = getMenuDAOBean;
         this.createMenusDTOBean = createMenusDTOBean;
     }
 
     // 메뉴 전체 조회
     public List<ResponseMenusGetDTO> exec(UUID boothId) {
         // boothId를 통해 원하는 부스 메뉴 전체 DAO 찾기
-        List<MenuDAO> menuDAOList = getMenuBean.getMenuDAOBean.exec(boothId, 1);
+        List<MenuDAO> menuDAOList = getMenuDAOBean.exec(boothId, 1);
         if(menuDAOList.isEmpty()) return null;
 
         // DAO 리스트를 DTO로 바꿔서 반환
