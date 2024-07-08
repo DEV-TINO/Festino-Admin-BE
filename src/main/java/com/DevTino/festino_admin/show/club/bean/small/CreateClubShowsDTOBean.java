042a53f40505192b6445b8ff3a3046b06d3a45a2
niireymik <yeah_0911@tukorea.ac.kr>
2024-07-08 15:32:46 +0900

    refactor : replace DTO-making part of CreateClubShowsDTOBean to CreateClubShowDTOBean

--- src/main/java/com/DevTino/festino_admin/show/club/bean/small/CreateClubShowsDTOBean.java
+++ src/main/java/com/DevTino/festino_admin/show/club/bean/small/CreateClubShowsDTOBean.java
@@ -2,6 +2,7 @@ package com.DevTino.festino_admin.show.club.bean.small;
 
 import com.DevTino.festino_admin.show.club.domain.ClubShowDAO;
 import com.DevTino.festino_admin.show.club.domain.DTO.ResponseClubShowGetDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -13,7 +14,13 @@ import java.util.List;
 @Component
 public class CreateClubShowsDTOBean {
 
-    static String zeroSec = ":00";
+    CreateClubShowDTOBean createClubShowDTOBean;
+    @Autowired
+    public CreateClubShowsDTOBean(CreateClubShowDTOBean createClubShowDTOBean){
+        this.createClubShowDTOBean = createClubShowDTOBean;
+    }
+
+
 
     // 동아리 공연 DTO List 생성
     public List<ResponseClubShowGetDTO> exec(List<ClubShowDAO> clubShowDAOList){
@@ -24,30 +31,8 @@ public class CreateClubShowsDTOBean {
         // DAO 리스트에서 DAO 하나씩 꺼내서
         for (ClubShowDAO clubShowDAO : clubShowDAOList){
 
-            // 공연 시작 날짜+시간을 LocalDateTime으로 변경
-            LocalDateTime showStart = LocalDateTime
-                    .parse(clubShowDAO.getShowDate() + clubShowDAO.getShowStartTime() + zeroSec,
-                            DateTimeFormatter.ofPattern("yyyy/MM/ddHH:mm:ss"));
-
-            // 공연 종료 날짜+시간을 LocalDateTime으로 변경
-            LocalDateTime showEnd = LocalDateTime
-                    .parse(clubShowDAO.getShowDate() + clubShowDAO.getShowEndTime() + zeroSec,
-                            DateTimeFormatter.ofPattern("yyyy/MM/ddHH:mm:ss"));
-
-            // 공연이 현재 진행 중인지 판단해서 isShowing 설정
-            Boolean isShowing =
-                    LocalDateTime.now().isAfter(showStart) && LocalDateTime.now().isBefore(showEnd);
-
-            // DTO 생성 -> DAO 값으로 설정
-            ResponseClubShowGetDTO dto = ResponseClubShowGetDTO.builder()
-                    .clubId(clubShowDAO.getClubId())
-                    .clubName(clubShowDAO.getClubName())
-                    .showDate(clubShowDAO.getShowDate())
-                    .showStartTime(clubShowDAO.getShowStartTime())
-                    .showEndTime(clubShowDAO.getShowEndTime())
-                    .isShowing(isShowing)
-                    .clubImage(clubShowDAO.getClubImage())
-                    .build();
+            // 동아리 공연 DTO 생성
+            ResponseClubShowGetDTO dto = createClubShowDTOBean.exec(clubShowDAO);
 
             // 생성한 DTO를 DTO 리스트에 삽입
             clubShowDTOList.add(dto);
