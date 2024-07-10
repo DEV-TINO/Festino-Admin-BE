9ec28b014c0a76d98912d0ca289e8c85c90ba4bd
niireymik <yeah_0911@tukorea.ac.kr>
2024-07-10 17:16:36 +0900

    fix : enum OrderType : remove wait_deposit, add is_deleted

--- src/main/java/com/DevTino/festino_admin/order/domain/OrderType.java
+++ src/main/java/com/DevTino/festino_admin/order/domain/OrderType.java
@@ -2,7 +2,7 @@ package com.DevTino.festino_admin.order.domain;
 
 public enum OrderType {
 
-    // 입금대기, 조리중, 조리완료
-    WAIT_DEPOSIT, COOKING, COOK_FINISHED
+    // 조리중, 조리완료, 주문취소
+    COOKING, COOK_FINISHED, IS_DELETED
 
 }
