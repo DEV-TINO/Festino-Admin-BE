cf6722b2dafb3f14e33bc1d5d919a6f3c04c84c6
niireymik <155354743+niireymik@users.noreply.github.com>
2024-07-18 20:25:29 +0900

    refactor : separate `CreateCookDAOsBean` from `UpdateOrderDepositBean`

--- src/main/java/com/DevTino/festino_admin/order/bean/UpdateOrderDepositBean.java
+++ src/main/java/com/DevTino/festino_admin/order/bean/UpdateOrderDepositBean.java
@@ -1,32 +1,25 @@
 package com.DevTino.festino_admin.order.bean;
 
 import com.DevTino.festino_admin.order.bean.small.*;
-import com.DevTino.festino_admin.order.domain.CookDAO;
 import com.DevTino.festino_admin.order.domain.DTO.RequestOrderDepositUpdateDTO;
 import com.DevTino.festino_admin.order.domain.DTO.ResponseOrderDepositUpdateDTO;
 import com.DevTino.festino_admin.order.domain.OrderDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-
 @Component
 public class UpdateOrderDepositBean {
 
     GetOrderDAOBean getOrderDAOBean;
+    CreateCookDAOsBean createCookDAOsBean;
     SaveOrderDAOBean saveOrderDAOBean;
-    CreateCookDAOBean createCookDAOBean;
-    SaveCookDAOBean saveCookDAOBean;
     CreateOrderDepositUpdateDTOBean createOrderDepositUpdateDTOBean;
 
     @Autowired
-    public UpdateOrderDepositBean(GetOrderDAOBean getOrderDAOBean, SaveOrderDAOBean saveOrderDAOBean, CreateCookDAOBean createCookDAOBean, SaveCookDAOBean saveCookDAOBean,CreateOrderDepositUpdateDTOBean createOrderDepositUpdateDTOBean){
+    public UpdateOrderDepositBean(GetOrderDAOBean getOrderDAOBean, CreateCookDAOsBean createCookDAOsBean, SaveOrderDAOBean saveOrderDAOBean, CreateOrderDepositUpdateDTOBean createOrderDepositUpdateDTOBean){
         this.getOrderDAOBean = getOrderDAOBean;
+        this.createCookDAOsBean = createCookDAOsBean;
         this.saveOrderDAOBean = saveOrderDAOBean;
-        this.createCookDAOBean = createCookDAOBean;
-        this.saveCookDAOBean = saveCookDAOBean;
         this.createOrderDepositUpdateDTOBean = createOrderDepositUpdateDTOBean;
     }
 
@@ -36,29 +29,17 @@ public class UpdateOrderDepositBean {
     public ResponseOrderDepositUpdateDTO exec(RequestOrderDepositUpdateDTO requestOrderDepositUpdateDTO){
 
         // orderId로 해당 Order DAO 찾기
-        UUID orderId = requestOrderDepositUpdateDTO.getOrderId();
-        OrderDAO orderDAO = getOrderDAOBean.exec(orderId);
+        OrderDAO orderDAO = getOrderDAOBean.exec(requestOrderDepositUpdateDTO.getOrderId());
         if (orderDAO == null) return null;
+
+        // orderDAO의 메뉴 정보에 따라 CookDAO 생성
+        createCookDAOsBean.exec(orderDAO);
 
         // DAO 수정
         orderDAO.setIsDeposit(true);
 
         // 수정된 DAO 저장
         saveOrderDAOBean.exec(orderDAO);
-
-        // 메뉴 정보 받기
-        List<Map<String, Object>> menuList = orderDAO.getMenuInfo();
-
-        // menuList에서 하나씩 꺼내서
-        for (Map<String, Object> menu : menuList){
-
-            // 메뉴 정보로 Cook DAO 생성
-            CookDAO cookDAO = createCookDAOBean.exec(menu, orderId);
-
-            // 생성한 Cook DAO 저장
-            saveCookDAOBean.exec(cookDAO);
-
-        }
 
         // 반환할 DTO 생성 및 반환
         return createOrderDepositUpdateDTOBean.exec(orderDAO);
---
+++ src/main/java/com/DevTino/festino_admin/order/bean/small/CreateCookDAOsBean.java
@@ -0,0 +1,45 @@
+package com.DevTino.festino_admin.order.bean.small;
+
+import com.DevTino.festino_admin.order.domain.CookDAO;
+import com.DevTino.festino_admin.order.domain.OrderDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class CreateCookDAOsBean {
+
+
+    CreateCookDAOBean createCookDAOBean;
+    SaveCookDAOBean saveCookDAOBean;
+
+    @Autowired
+    public CreateCookDAOsBean(CreateCookDAOBean createCookDAOBean, SaveCookDAOBean saveCookDAOBean){
+        this.createCookDAOBean = createCookDAOBean;
+        this.saveCookDAOBean = saveCookDAOBean;
+    }
+
+
+
+    // orderDAO의 메뉴 정보에 따라 CookDAO 생성
+    public void exec(OrderDAO orderDAO){
+
+        // 메뉴 정보 받기
+        List<Map<String, Object>> menuList = orderDAO.getMenuInfo();
+
+        // menuList에서 하나씩 꺼내서
+        for (Map<String, Object> menu : menuList){
+
+            // 메뉴 정보로 Cook DAO 생성
+            CookDAO cookDAO = createCookDAOBean.exec(menu, orderDAO.getOrderId());
+
+            // 생성한 Cook DAO 저장
+            saveCookDAOBean.exec(cookDAO);
+
+        }
+
+    }
+
+}
