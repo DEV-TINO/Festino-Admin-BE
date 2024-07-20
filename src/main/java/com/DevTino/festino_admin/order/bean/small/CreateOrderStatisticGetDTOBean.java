4a0cc32b20d56af8b14e59370139cf4eb80397ce
niireymik <155354743+niireymik@users.noreply.github.com>
2024-07-21 03:53:40 +0900

    comment : add more detailed explanation in `CreateOrderStatisticsGetDTOBean`

--- src/main/java/com/DevTino/festino_admin/order/bean/small/CreateOrderStatisticGetDTOBean.java
+++ src/main/java/com/DevTino/festino_admin/order/bean/small/CreateOrderStatisticGetDTOBean.java
@@ -36,7 +36,7 @@ public class CreateOrderStatisticGetDTOBean {
             // 메뉴의 총판매수량
             Integer menuCount = 0;
 
-            // 메뉴 이름과 날짜(date)로 Cook 조회
+            // 메뉴 이름과 날짜(date)로 Cook 조회 : Cook은 입금 확인과 동시에 생성되므로 완료여부와 관계 없이 모두 통계 반영
             List<CookDAO> cookDAOList = getCooksDAOBean.exec(menuDAO.getMenuName(), date);
 
             // 조회한 Cook 리스트로 메뉴의 총판매수량 계산
