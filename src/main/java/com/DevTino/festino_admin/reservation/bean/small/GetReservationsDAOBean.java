6f2a5c5490ea3cadc005405cbc2b0062f8bbb527
yoosuhyeon <dbtngus20@naver.com>
2024-07-20 16:09:12 +0900

    fix : add date to bean

--- src/main/java/com/DevTino/festino_admin/reservation/bean/small/GetReservationsDAOBean.java
+++ src/main/java/com/DevTino/festino_admin/reservation/bean/small/GetReservationsDAOBean.java
@@ -12,22 +12,17 @@ import java.util.UUID;
 @Component
 public class GetReservationsDAOBean {
     ReservationRepositoryJPA reservationRepositoryJPA;
-    CheckReservationDAOBean checkReservationDAOBean;
 
     @Autowired
-    public GetReservationsDAOBean(ReservationRepositoryJPA reservationRepositoryJPA, CheckReservationDAOBean checkReservationDAOBean) {
+    public GetReservationsDAOBean(ReservationRepositoryJPA reservationRepositoryJPA) {
         this.reservationRepositoryJPA = reservationRepositoryJPA;
-        this.checkReservationDAOBean = checkReservationDAOBean;
     }
 
     // boothId를 통해 원하는 부스 전체 DAO 오래된 순으로 반환
     @Transactional(readOnly = true)
-    public List<ReservationDAO> exec(UUID boothId, String type) {
+    public List<ReservationDAO> exec(UUID boothId, String type, Integer date) {
         // 예약 리스트 생성
         List<ReservationDAO> reservationDAOList;
-
-        // 부스의 오픈 시간을 활용하여 몇일차인지 구분
-        Integer date = checkReservationDAOBean.exec(boothId);
 
         // 예약 목록일 때
         if (type.equals("all"))
