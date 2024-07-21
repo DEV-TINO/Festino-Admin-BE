9c4d6c8c2f56c17aec4a56dfd2887481c4cccec0
niireymik <155354743+niireymik@users.noreply.github.com>
2024-07-21 19:48:59 +0900

    fix : set `boothId` when the CookDAO is created

--- src/main/java/com/DevTino/festino_admin/order/bean/small/CreateCookDAOBean.java
+++ src/main/java/com/DevTino/festino_admin/order/bean/small/CreateCookDAOBean.java
@@ -11,11 +11,13 @@ import java.util.UUID;
 @Component
 public class CreateCookDAOBean {
 
+    // DAO 생성해 반환
     public CookDAO exec(Map<String, Object> menu, OrderDAO orderDAO){
 
         return CookDAO.builder()
                 .cookId(UUID.randomUUID())
                 .orderId(orderDAO.getOrderId())
+                .boothId(orderDAO.getBoothId())
                 .date(orderDAO.getDate())
                 .menuName((String) menu.get("menuName"))
                 .totalCount((Integer) menu.get("menuCount"))
