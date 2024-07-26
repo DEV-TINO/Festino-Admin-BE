66bda16709945bfbbcf48314fc8c2d98cf57ce0e
Lee <lsmin476@naver.com>
2024-07-27 00:58:47 +0900

    fix: night booth get logic transaction add

--- src/main/java/com/DevTino/festino_admin/booth/bean/small/GetNightBoothDAOBean.java
+++ src/main/java/com/DevTino/festino_admin/booth/bean/small/GetNightBoothDAOBean.java
@@ -2,8 +2,11 @@ package com.DevTino.festino_admin.booth.bean.small;
 
 import com.DevTino.festino_admin.booth.domain.NightBoothDAO;
 import com.DevTino.festino_admin.booth.repository.NightBoothRepositoryJPA;
+import jakarta.persistence.LockModeType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +21,8 @@ public class GetNightBoothDAOBean {
     }
 
     // 부스 아이디로 DAO 찾아서 반환
+    @Transactional
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     public NightBoothDAO exec(UUID boothId) {
         return nightBoothRepository.findById(boothId).orElse(null);
     }
