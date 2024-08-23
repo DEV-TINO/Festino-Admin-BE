8581609b287aec5f846e49c61c9035bd8e513cd4
rvbear <rvbear2000@gmail.com>
2024-08-24 01:38:58 +0900

    feat: password encryption

--- src/main/java/com/DevTino/festino_admin/user/bean/small/CreateUserDAOBean.java
+++ src/main/java/com/DevTino/festino_admin/user/bean/small/CreateUserDAOBean.java
@@ -2,6 +2,7 @@ package com.DevTino.festino_admin.user.bean.small;
 
 import com.DevTino.festino_admin.user.domain.DTO.RequestUserSaveDTO;
 import com.DevTino.festino_admin.user.domain.UserDAO;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -15,7 +16,7 @@ public class CreateUserDAOBean {
                 .userId(UUID.randomUUID())
                 .adminId(requestUserSaveDTO.getAdminId())
                 .adminName(requestUserSaveDTO.getAdminName())
-                .passWord(requestUserSaveDTO.getPassWord())
+                .passWord(BCrypt.hashpw(requestUserSaveDTO.getPassWord(), BCrypt.gensalt()))
                 .role(requestUserSaveDTO.getRole())
                 .adminCategory(requestUserSaveDTO.getCategory())
                 .build();
--- src/main/java/com/DevTino/festino_admin/user/bean/small/GetUserDAOBean.java
+++ src/main/java/com/DevTino/festino_admin/user/bean/small/GetUserDAOBean.java
@@ -3,6 +3,7 @@ package com.DevTino.festino_admin.user.bean.small;
 import com.DevTino.festino_admin.user.domain.DTO.RequestUserLoginDTO;
 import com.DevTino.festino_admin.user.domain.UserDAO;
 import com.DevTino.festino_admin.user.repository.UserRepositoryJPA;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +25,13 @@ public class GetUserDAOBean {
 
     // adminId와 passWord로 특정 유저 조회
     public UserDAO exec(RequestUserLoginDTO requestUserLoginDTO) {
-        return userRepositoryJPA.findByAdminIdAndPassWord(requestUserLoginDTO.getAdminId(), requestUserLoginDTO.getPassWord());
+        UserDAO userDAO = userRepositoryJPA.findByAdminId(requestUserLoginDTO.getAdminId());
+
+        // accountId로 조회했을 때 값이 없는 경우
+        if(userDAO == null) return null;
+
+        // 암호화된 비밀번호를 확인했을 때 일치한다면 true, 다르다면 false
+        boolean flag = BCrypt.checkpw(requestUserLoginDTO.getPassWord(), userDAO.getPassWord());
+        return flag ? userDAO : null;
     }
 }
