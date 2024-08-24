16c5db51f795258cbbd94ce88d28ea94140919c5
yoosuhyeon <dbtngus20@naver.com>
2024-08-25 02:25:15 +0900

    style : change exception handling

--- src/main/java/com/DevTino/festino_admin/order/bean/GetOrderAllBean.java
+++ src/main/java/com/DevTino/festino_admin/order/bean/GetOrderAllBean.java
@@ -30,7 +30,7 @@ public class GetOrderAllBean {
     public List<ResponseOrderAllGetDTO> exec(UUID boothId, Integer date){
         // 주문한 학과 구분
         String adminName = getOrderBoothNameDAOBean.exec(boothId);
-        if(adminName.isEmpty()) return new ArrayList<>();
+        if(adminName.isEmpty()) return null;
 
         // 날짜의 Order 오래된순 전체 조회 -> DAO 리스트
         List<OrderDTO> dtoList = getOrdersDAOBean.exec(adminName, date);
--- src/main/java/com/DevTino/festino_admin/order/bean/GetOrderCancelBean.java
+++ src/main/java/com/DevTino/festino_admin/order/bean/GetOrderCancelBean.java
@@ -30,7 +30,7 @@ public class GetOrderCancelBean {
     public List<ResponseOrderCancelGetDTO> exec(UUID boothId, Integer date){
         // 주문한 학과 구분
         String adminName = getOrderBoothNameDAOBean.exec(boothId);
-        if(adminName.isEmpty()) return new ArrayList<>();
+        if(adminName.isEmpty()) return null;
 
         // 날짜의 취소된 Order 오래된순 전체 조회 -> DAO 리스트
         List<OrderDTO> dtoList = getOrderCancelDAOBean.exec(adminName, date);
