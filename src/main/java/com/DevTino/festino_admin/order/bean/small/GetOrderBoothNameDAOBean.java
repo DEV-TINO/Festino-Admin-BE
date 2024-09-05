3d111d789ba66c6b955ab94514acaa03c75e0279
yoosuhyeon <dbtngus20@naver.com>
2024-09-05 21:59:01 +0900

    style : change adminName notation

--- src/main/java/com/DevTino/festino_admin/order/bean/small/GetOrderBoothNameDAOBean.java
+++ src/main/java/com/DevTino/festino_admin/order/bean/small/GetOrderBoothNameDAOBean.java
@@ -12,7 +12,7 @@ public class GetOrderBoothNameDAOBean {
         return switch (boothId.toString()) {
             case "bcb6ddc2-1116-4729-a643-fa8f3bb5408f" -> "computer";
             case "8414328c-f765-4741-bf22-59f5a52e06bf" -> "game";
-            case "3aacd1de-57c1-4685-a94a-63c1664f51bf" -> "new_material";
+            case "3aacd1de-57c1-4685-a94a-63c1664f51bf" -> "newMaterial";
             case "71ced602-eb69-4e0e-96cd-3bcded85fd76" -> "machine";
             case "5167a573-3a5f-4b40-ab9e-22c8fdf16c84" -> "energy";
             case "f0fe6e61-e530-4606-a003-cf224833b11b" -> "electronics";
