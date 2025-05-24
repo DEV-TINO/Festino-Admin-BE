34e3faab59b9e8924ecea576191692923086bb4e
niireymik <niireymik@gmail.com>
2025-05-24 18:17:51 +0900

    fix : add biochemistry cook

--- src/main/java/com/DevTino/festino_admin/order/repository/BiochemistryCookRepositoryImpl.java
+++ src/main/java/com/DevTino/festino_admin/order/repository/BiochemistryCookRepositoryImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Component("biochemistryCook")
-public class BiochemistryCookRepositoryImpl {
+public class BiochemistryCookRepositoryImpl implements CookRepository<BiochemistryCookDAO>{
 
     BiochemistryCookRepositoryJPA biochemistryCookRepositoryJPA;
 
